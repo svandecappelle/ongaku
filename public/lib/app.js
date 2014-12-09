c19271b2a8c82e6afa2044e2afe1af1ab105c9c0
svandecappelle <steeve.vandecappelle@opti-mix.fr>
2014-12-09 10:25:24 +0100

    Playlist plays next song auto when finished current

--- public/lib/app.js
+++ public/lib/app.js
@@ -8,11 +8,26 @@
 	$.ongaku = new Player();
 
 	Player.prototype.play = function(uid){
+		$(".playing").removeClass('playing');
+		this.current = uid;
+
 		$("#controls").attr("src", "/stream/".concat(uid));
 		$("#mp3src").attr("src", "/stream/".concat(uid)).remove().appendTo("#controls");
 		$("#controls")[0].pause();
 		$("#controls")[0].load();
 		$("#controls")[0].play();
+
+		$(".play").find("[data-uid='" + this.current + "']").parent().parent().addClass('playing');
+	};
+
+	Player.prototype.next = function(){		
+		var nextSong = $(".play").find("[data-uid='" + this.current + "']").parent().parent().next();
+		if (nextSong){
+			var nextUid = nextSong.find(".button").data('uid');
+			if (nextUid){
+				this.play(nextUid);
+			}
+		}
 	};
 
 	function Controls(){
@@ -21,6 +36,7 @@
 
 	Controls.prototype.bind = function(){
 		$(".play .button").click(function(){
+			$(this)
 			$.ongaku.play($(this).data("uid"));
 		});
 		$('a.song').click(function(){
