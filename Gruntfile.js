5b0e0c389eeb2b07564cbfd27bc70e9df56b9cef
Steeve Vandecappelle <me@mizore.fr>
2017-08-30 09:38:35 +0200

    doc + nw

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   grunt.initConfig({
-	  jshint: {
+    jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'public/lib/**/*.js'],
       options: {
         globals: {
@@ -11,7 +10,7 @@ module.exports = function(grunt) {
     },
     concat: {
       css: {
-        src : ['public/assets/main.min.css', 'public/assets/chat.min.css', 'public/assets/media-theme.min.css', 'public/assets/video-js-ongaku-theme.min.css'],
+        src: ['public/assets/main.min.css', 'public/assets/chat.min.css', 'public/assets/media-theme.min.css', 'public/assets/video-js-ongaku-theme.min.css'],
         dest: 'public/assets/application.min.css'
       },
       js: {
@@ -20,20 +19,20 @@ module.exports = function(grunt) {
       }
     },
     cssmin: {
-        target: {
-            files: grunt.file.expandMapping(['public/lib/*.css'], 'public/assets/', {
-                rename: function (destBase, destPath) {
-                    return destBase + destPath.replace("public/lib/", "").replace('.css', '.min.css');
-                }
-            })
-        }
+      target: {
+        files: grunt.file.expandMapping(['public/lib/*.css'], 'public/assets/', {
+          rename: function (destBase, destPath) {
+            return destBase + destPath.replace('public/lib/', '').replace('.css', '.min.css');
+          }
+        })
+      }
     },
     uglify: {
       prod: {
         files: grunt.file.expandMapping(['public/lib/*.js'], 'public/assets/', {
-            rename: function (destBase, destPath) {
-                return destBase + destPath.replace("public/lib/", "").replace('.js', '.min.js');
-            }
+          rename: function (destBase, destPath) {
+            return destBase + destPath.replace('public/lib/', '').replace('.js', '.min.js');
+          }
         }),
         options: {
           mangle: false
@@ -47,17 +46,27 @@ module.exports = function(grunt) {
           output: 'docs/'
         }
       }
+    },
+    nwjs: {
+      options: {
+        platforms: ['linux'],
+        buildDir: './build',
+        flavor: 'normal'
+      },
+      src: ['./**/*'] // Your node-webkit app
     }
   });
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-docco-dir');
-
+  grunt.loadNpmTasks('grunt-nw-builder');
 
   grunt.registerTask('doc', ['concat']);
   grunt.registerTask('quality', ['concat']);
   grunt.registerTask('default', ['uglify', 'cssmin', 'concat']);
+  grunt.registerTask('desktop', ['nwjs']);
 };
